a1986048436626477cf22bbc2b3b75467719f81f
conoroheocha <conoroheocha@gmail.com>
2022-04-21 13:54:19 +0100

    fixed dependencies

--- src/Submit.js
+++ src/Submit.js
@@ -2,8 +2,7 @@ import React, { useState, Component } from "react";
 import DateTimePicker from 'react-datetime-picker';
 import DatePicker from "react-date-picker";
 import { Col, Container } from "react-bootstrap";
-import { Button } from "bootstrap";
-import {Map, TitleLayer} from "react-leaflet";
+import { Map, TitleLayer } from "react-leaflet";
 import osm from "./osm-providers";
 import "leaflet/dist/leaflet.css";
 
@@ -14,7 +13,7 @@ function Submit() {
     const [message, setMessage] = useState("");
     const [value, onChange] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(new Date());
-    const[center, setCenter] = useState({lat: 40.730610, lng: -73.935242});
+    const [center, setCenter] = useState({ lat: 40.730610, lng: -73.935242 });
     const ZOOM_LEVEL = 9;
     // eslint-disable-next-line no-undef
     const mapRef = useRef();
@@ -46,37 +45,37 @@ function Submit() {
         <div className="Submit">
             <Col fluid>
 
-                <DateTimePicker selected={selectedDate} 
-                onChange={onChange} value={value}
-                dateFormat = 'dd/MM/yyyy HH:mm'
-                minDate={new Date()}
-                isClearable
-                showYearDropdown
-                showMonthDropdown
-                scrollableMonthYearDropdown
+                <DateTimePicker selected={selectedDate}
+                    onChange={onChange} value={value}
+                    dateFormat='dd/MM/yyyy HH:mm'
+                    minDate={new Date()}
+                    isClearable
+                    showYearDropdown
+                    showMonthDropdown
+                    scrollableMonthYearDropdown
                 />
 
-                
-                    
-                    <div classname="row">
-                        <div classname="col text-center">
-                            <h2>Openstreet Maps</h2>
-                            <p>Loading maps</p>
-                            <div className="col">
-                                <Map
-                                    center={center}
-                                    zoom={ZOOM_LEVEL}
-                                    ref={mapRef}
-                                >
-                                    <TitleLayer 
-                                        url={osm.maptiler.url} 
-                                        attribution={osm.maptiler.attribution} />
-                                </Map>
-                            </div>
+
+
+                <div classname="row">
+                    <div classname="col text-center">
+                        <h2>Openstreet Maps</h2>
+                        <p>Loading maps</p>
+                        <div className="col">
+                            <Map
+                                center={center}
+                                zoom={ZOOM_LEVEL}
+                                ref={mapRef}
+                            >
+                                <TitleLayer
+                                    url={osm.maptiler.url}
+                                    attribution={osm.maptiler.attribution} />
+                            </Map>
                         </div>
                     </div>
+                </div>
 
-               
+
 
                 <form onSubmit={handleSubmit}>
                     <button type="submit">Submit</button>
